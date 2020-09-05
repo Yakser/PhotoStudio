9884211ad21071a8698d81b2ea75f85bcde1d66f
Sergey Yaksanov <53031664+Yakser@users.noreply.github.com>
2020-09-05 20:51:22 +0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -19,3 +19,38 @@ function toggle() {
 	sec.classList.toggle("active");
 	nav.classList.toggle("active");
 }
+
+// MAIN NAV
+
+let linksList = document.querySelectorAll(".nav__link");
+let blocksList = document.querySelectorAll(".content__block");
+
+let ind = 0;
+
+for(let i = 0; i < linksList.length; i++){
+
+	linksList[i].addEventListener("click", function() {
+
+		blocksList[ind].style.display = "none";
+		blocksList[i].style.display = "block";
+		
+		linksList[ind].classList.remove("active-link");
+		linksList[i].classList.add("active-link");
+		ind = i;
+		toggle();
+
+	});
+}
+
+let moreBtn = document.getElementById("know-more");
+moreBtn.addEventListener("click", function() {
+
+		blocksList[ind].style.display = "none";
+		blocksList[1].style.display = "block";
+		
+		linksList[ind].classList.remove("active-link");
+		linksList[1].classList.add("active-link");
+		ind = 1;
+		
+
+	});
